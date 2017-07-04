d06350353827264249d1908faf4b845e1bbf7d4d
Andrade, Raphael <raphael.andrade@mastercard.com>
2017-07-04 00:01:16 -0300

    Commit for rookies

---
+++ src/main/java/com/mastercard/MyResource.java
@@ -0,0 +1,31 @@
+package com.mastercard;
+
+import com.mastercard.model.User;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+
+}
